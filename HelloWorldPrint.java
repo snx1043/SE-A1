30b75138641e169b38b0e73e7225c24e7547f7ed
snx1043 <snx1043@autuni.ac.nz>
2020-04-24 22:00:55 +1200

    Added HelloWordPrint

---
+++ HelloWorldPrint.java
@@ -0,0 +1,11 @@
+package HelloWorld;
+
+public class HelloWorldPrint 
+{
+    private String printText = "Hello WOrld";
+    
+    public void printString()
+    {
+        System.out.println(printText);
+    }
+}
