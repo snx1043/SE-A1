7da8ca44dc3aa6866a4aef20bd016166dfa268fe
Phaisan1233 <42394385+Phaisan1233@users.noreply.github.com>
2020-04-24 23:22:05 +1200

    -

---
+++ index.js
@@ -0,0 +1,29 @@
+// console.log('hello world');
+// let num = 0;
+// num++;
+console.log('ggg');
+
+// const demo = require('./demo');
+// console.log(demo);
+// console.log(demo.sum1(1,5));
+// console.log(demo.sqare(5));
+
+// const event1 = require('events');
+// const events = new event1();
+
+// events.on('r', () =>{
+//     console.log('1');
+// })
+// // event1.on('hi', () =>{
+// //     console.log('3')
+// // })
+
+// events.emit('r');
+// // event1.emit('hi');
+
+// const readline = require('rea')
+// document.get
+
+//console.log(window);
+var m = 'df';
+console.log(m);
