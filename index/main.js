2b7ea5c5b572aa9fe7589e3d0f7bf7ee09437d52
Phaisan1233 <42394385+Phaisan1233@users.noreply.github.com>
2020-04-25 23:04:52 +1200

    add new function

--- index/main.js
+++ index/main.js
@@ -20,8 +20,8 @@
 //     rl.close()
 //   }});
 
-const h1 = document.getElementById('h1');
-const p1 = document.getElementById('p1');
+// const h1 = document.getElementById('h1');
+// const p1 = document.getElementById('p1');
 
-p1.innerText= '';
-console.l
+// p1.innerText= '';
+// console.l
